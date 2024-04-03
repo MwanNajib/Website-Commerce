2f837b229d61b1d78f0fd55cce586283a6edd5c6
Rizki Putri <rizputfy@gmail.com>
2024-04-03 20:28:00 +0700

    edit heroes

--- script.js
+++ script.js
@@ -2,6 +2,35 @@ $(document).ready(function () {
     $('.navbar').hide().slideDown('smooth');
 });
 
+document.querySelector('.order-btn').addEventListener('click', function() {
+    document.querySelector('#order-section').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
+window.addEventListener('scroll', function() {
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    var fadeoutValue = Math.min(scrollTop / 500, 1); // Adjust the value based on your preference
+
+    if (fadeoutValue < 1) {
+        document.getElementById('heroes').classList.remove('fadeout');
+        document.getElementById('heroes').style.background = 'linear-gradient(180deg, #FFFF00 0%, #ffffff 100%)';
+    } else {
+        document.getElementById('heroes').classList.add('fadeout');
+    }
+});
+
+$(document).ready(function() {
+    $('.order-btn').click(function() {
+        var produkTop = $('#produk').offset().top;
+
+        $('html, body').animate({
+            scrollTop: produkTop
+        }, 800);
+    });
+});
+
+
 $(document).ready(function () {
     $("#fadeInDiv").hide().fadeIn(1000);
 });
