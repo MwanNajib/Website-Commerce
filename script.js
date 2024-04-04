914c4e46d4c9691615e0d3684f00800255799994
Tri Kusuma Faradila <faradilatrikusuma@gmail.com>
2024-04-04 11:10:10 +0700

    merapikan section services dan contact

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 $(document).ready(function () {
     $('.navbar').hide().slideDown('smooth');
 
-    $('.order-btn').click(function() {
+    $('.order-btn').click(function () {
         $('html, body').animate({
             scrollTop: $('#produk').offset().top
         }, 500);
     });
 
-    $('.promo-btn').click(function() {
+    $('.promo-btn').click(function () {
         $('html, body').animate({
             scrollTop: $('#layanan-kami').offset().top
         }, 500);
     });
 
     $(".card-promo").hover(
-        function() {
-            $(this).css("border-color", "#ffc107"); 
-            $(this).css("box-shadow", "0 0 20px rgba(0, 0, 0, 0.1)"); 
+        function () {
+            $(this).css("border-color", "#ffc107");
+            $(this).css("box-shadow", "0 0 20px rgba(0, 0, 0, 0.1)");
         },
-        function() {
-            $(this).css("border-color", ""); 
-            $(this).css("box-shadow", ""); 
+        function () {
+            $(this).css("border-color", "");
+            $(this).css("box-shadow", "");
         }
     );
-    
-    $('.btn-promo').click(function() {
+
+    $('.btn-promo').click(function () {
         $('html, body').animate({
             scrollTop: $('#produk').offset().top
         }, 500);
@@ -32,13 +32,13 @@ $(document).ready(function () {
 
 });
 
-document.querySelector('.order-btn').addEventListener('click', function() {
+document.querySelector('.order-btn').addEventListener('click', function () {
     document.querySelector('#order-section').scrollIntoView({
         behavior: 'smooth'
     });
 });
 
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
     var scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
     var fadeoutValue = Math.min(scrollTop / 500, 1); // Adjust the value based on your preference
 
